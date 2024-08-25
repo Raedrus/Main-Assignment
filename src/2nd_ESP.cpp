949f4fd8b258c46d80b1ff0d11e8fdc621a174f9
HueXiang <hueyungxiang@gmail.com>
2024-08-26 01:38:56 +0800

    Update 2nd_ESP.cpp

--- src/2nd_ESP.cpp
+++ src/2nd_ESP.cpp
@@ -390,6 +390,9 @@ void Registration(){
     i=0;
     ID=0;
     //Animal ID input
+    i=2;
+    ID=0;
+    //Animal ID input
     while(key!='A'){
         lcd.setCursor(0, 0);
         lcd.printf("Animal ID: %.3",ID);
@@ -404,7 +407,7 @@ void Registration(){
 
         else if (int(key)>=0 && int(key)<=9){
             ID+=int(key)*pow(10,i);
-            i++;
+            i--;
             delay(200);
         }  
     }
